d24ac2d5a41e83f7bb30ccba4b5cdaaf0a0ded10
root <root@LAPTOP-C9DKO0J9.localdomain>
2023-05-23 14:01:14 +0700

    first commit

---
+++ src/resources/views/sample.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.main')
+
+@section('content')
+<center>
+<div class="card" style="width: 18rem; ">
+  <img src="https://i.pinimg.com/736x/c5/65/e9/c565e97e6454c74baad52a20c1ff5f20.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">garasi drift</h5>
+    <p class="card-text">pandu bashir</p>
+
+    <!-- <a href="#" class="btn btn-primary">Go somewhere</a> -->
+  </div>
+</div>
+</center>
+
+@endsection
